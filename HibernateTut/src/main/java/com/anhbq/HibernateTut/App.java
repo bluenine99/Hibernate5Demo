7fd418994f59865104a6fadd2ea49d653c84ee1c
AnhQuoc <anhbq@mobifone.vn>
2018-05-24 11:16:17 +0700

    The second commit

---
+++ HibernateTut/src/main/java/com/anhbq/HibernateTut/App.java
@@ -0,0 +1,13 @@
+package com.anhbq.HibernateTut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
